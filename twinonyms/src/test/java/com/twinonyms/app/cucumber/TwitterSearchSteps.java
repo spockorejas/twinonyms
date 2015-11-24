a62aaf3194b0e0ed9b447035ec3e99747500a239
spockorejas <spockorejas@gmail.com>
2015-11-25 00:04:54 +0100

    Added cucumber dependency to maven and also Steps for twitter search. Not yet implemented :P

---
+++ twinonyms/src/test/java/com/twinonyms/app/cucumber/TwitterSearchSteps.java
@@ -0,0 +1,26 @@
+package com.twinonyms.app.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TwitterSearchSteps {
+    @Given("^a term$")
+    public void aTerm() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the term is sent to the Search Api$")
+    public void theTermIsSentToTheSearchApi() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^a list of tweets is retrieved$")
+    public void aListOfTweetsIsRetrieved() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
